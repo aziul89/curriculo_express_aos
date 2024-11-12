5e883dbeaca78b257e6ea94bcc7f26bab0e17339
Maria Luiza Farias <130606221+aziul89@users.noreply.github.com>
2024-11-11 23:03:27 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const routes = require('./routes');
 
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use('/api', routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
